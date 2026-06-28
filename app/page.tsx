6b69bf0e6674c24f76a32e4109238ffc1a9adddd
Lalit Kumar <Lalitk52221@outlook.com>
2026-06-28 22:07:56 +0530

    Added product section

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Categories from "./components/Categories";
+import Products from "./components/Products";
 // import Contact from "./components/Contact";
 // import Customers from "./components/Customers";
 // import Footer from "./components/Footer";
 // import Infrastructure from "./components/Infrastructure";
-// import Products from "./components/Products";
 // import WhatsAppFloat from "./components/WhatsAppFloat";
 // import WhyUs from "./components/WhyUs";
 
@@ -19,7 +19,7 @@ function Home() {
       <Hero/>
       <Categories/>
       <About/>
-      {/* <Products/> */}
+      <Products/>
       {/* <Infrastructure/> */}
       {/* <WhyUs/> */}
       {/* <Customers/> */}
